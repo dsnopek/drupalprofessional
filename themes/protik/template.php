abab6d0694ca3ad6aad170ca111f29d381aac488
David Snopek <dsnopek@gmail.com>
2012-12-26 22:29:55 +0000

    Theme changes, including fixing support for 'color'.

--- themes/protik/template.php
+++ themes/protik/template.php
@@ -1,7 +1,25 @@
 <?php
 
-function protik_preprocess_page(&$vars) {
+/**
+ * Override or insert variables into the page template for HTML output.
+ */
+function protik_process_html(&$variables) {
+  // Hook into color.module.
+  if (module_exists('color')) {
+    _color_html_alter($variables);
+  }
+}
+
+/**
+ * Override or insert variables into the page template.
+ */
+function protik_process_page(&$variables) {
+  // Hook into color.module.
+  if (module_exists('color')) {
+    _color_page_alter($variables);
+  }
+
   // get rid of breadcrumbs
-  $vars['breadcrumb'] = FALSE;
+  $variables['breadcrumb'] = FALSE;
 }
 
